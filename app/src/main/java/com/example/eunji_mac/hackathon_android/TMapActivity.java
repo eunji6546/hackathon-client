598651eace3b0976a35487e72a177bc385d9539d
eunji6546 <eunji6546@gmail.com>
2016-08-23 02:40:59 +0900

    pay tmap recommand

--- app/src/main/java/com/example/eunji_mac/hackathon_android/TMapActivity.java
+++ app/src/main/java/com/example/eunji_mac/hackathon_android/TMapActivity.java
@@ -46,7 +46,7 @@ public class TMapActivity extends AppCompatActivity {
 
         //mMapView.setCompassMode(true);
         mMapView.setTrackingMode(true);
-        mMapView.setZoomLevel(4);
+        mMapView.setZoomLevel(15);
         //mMapView.setLocationPoint(126.985022,37.566474);
         //mMapView.setLocationPoint(127.350827,36.367394);
 
@@ -65,28 +65,33 @@ public class TMapActivity extends AppCompatActivity {
             TMapPoint one = new TMapPoint(lat,lon);
             tPoints.add(one);
 
-            TMapMarkerItem tMapMarkerItem = new TMapMarkerItem();
-            tMapMarkerItem.setTMapPoint(one);
-            tMapMarkerItem.setCalloutTitle(mNames.get(i));
-            tMapMarkerItem.setAutoCalloutVisible(true);
-            Bitmap icon = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.poi_dot);
-            tMapMarkerItem.setIcon(icon);
-            mMapView.addMarkerItem(mNames.get(i),tMapMarkerItem);
-
         }
         TMapInfo tMapInfo = mMapView.getDisplayTMapInfo(tPoints);
         TMapPoint center = tMapInfo.getTMapPoint();
         TMapMarkerItem tMapMarkerItem = new TMapMarkerItem();
         tMapMarkerItem.setTMapPoint(center);
+        Log.e("Center is",center.getLatitude()+"/"+center.getLongitude());
         tMapMarkerItem.setCalloutTitle(keyword);
         tMapMarkerItem.setAutoCalloutVisible(true);
         Bitmap icon = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.poi_star);
         tMapMarkerItem.setIcon(icon);
 
+
         mMapView.setLocationPoint(center.getLongitude(),center.getLatitude());
-        mMapView.setCenterPoint(center.getLongitude(),center.getLatitude());
+        //mMapView.setCenterPoint(center.getLongitude(),center.getLatitude());
         mMapView.setZoomLevel(tMapInfo.getTMapZoomLevel());
         Log.e("g", String.valueOf(tMapInfo.getTMapZoomLevel()));
+
+        for (int i =0; i<mNames.size(); i++){
+
+            TMapMarkerItem tMapMarkerItem1 = new TMapMarkerItem();
+            tMapMarkerItem1.setTMapPoint(tPoints.get(i));
+            tMapMarkerItem1.setCalloutTitle(mNames.get(i));
+            tMapMarkerItem1.setAutoCalloutVisible(true);
+            Bitmap icon1 = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.poi_dot);
+            tMapMarkerItem1.setIcon(icon1);
+            mMapView.addMarkerItem(mNames.get(i),tMapMarkerItem1);
+        }
 
         frameLayout.addView(mMapView);
 
