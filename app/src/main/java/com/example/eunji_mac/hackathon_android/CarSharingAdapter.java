731e20ee4ac79b3a876e8ebc203e916ee886ff95
jisu0123 <jisu0123@kaist.ac.kr>
2016-08-22 17:45:48 +0900

    Finish the 6th scene!!

--- app/src/main/java/com/example/eunji_mac/hackathon_android/CarSharingAdapter.java
+++ app/src/main/java/com/example/eunji_mac/hackathon_android/CarSharingAdapter.java
@@ -1,6 +1,7 @@
 package com.example.eunji_mac.hackathon_android;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,7 +10,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -18,6 +23,7 @@ import java.util.ArrayList;
 public class CarSharingAdapter extends ArrayAdapter {
 
     private ArrayList<String> items;
+    private TextView mText1;
 
     public CarSharingAdapter(Context context, int resourceid, ArrayList<String> objects) {
         super(context, resourceid, objects);
@@ -26,21 +32,47 @@ public class CarSharingAdapter extends ArrayAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         View v = convertView;
+        final UrlConnection url = new UrlConnection();
 
         if (v == null) {
             LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             v = vi.inflate(R.layout.items, null);
         }
 
-        TextView mText1 = (TextView) v.findViewById(R.id.itemtext);
+        mText1 = (TextView) v.findViewById(R.id.itemtext);
         TextView mText2 = (TextView) v.findViewById(R.id.idtext);
+        TextView mRide = (TextView) v.findViewById(R.id.button);
+
         mText1.setText(items.get(position).split("\\+")[0]);
         mText2.setText(items.get(position).split("\\+")[1]);
 
         Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Stark.OTF");
 
         mText1.setTypeface(tf);
+        mRide.setTypeface(tf);
+
         Log.v("ID : ", mText2.getText().toString());
+
+        Button button = (Button) v.findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mAvailable = mText1.getText().toString().split("\n")[4];
+                String mParse1 = mAvailable.split("\\[")[1];
+                String current = mParse1.split("/")[0];
+                String full = mParse1.split("/")[1].split("\\]")[0];
+                if (current.equals(full)) {
+                    Toast.makeText(getContext(), "탑승이 마감되었습니다!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(getContext(), "탑승이 신청되었습니다!", Toast.LENGTH_SHORT).show();
+                int mPeople = Integer.parseInt(current) + 1;
+                int mFull = Integer.parseInt(full);
+                String restinfo = mText1.getText().toString().split("Av")[0];
+                mText1.setText(restinfo + "Available. "+"[" + mPeople + "/" + mFull +"]");
+            }
+        });
+
         return v;
     }
 }
